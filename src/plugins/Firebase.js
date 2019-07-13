eb741ed0c5330eca285f9ecfea9da8751f52d817
jeong369 <injeong319@gmail.com>
2019-07-13 18:11:44 +0900

    firebase services 사용할 js파일 생성

---
+++ src/plugins/Firebase.js
@@ -0,0 +1,15 @@
+import firebase from 'firebase/app'
+
+// setup firebase
+const Config = {
+    apiKey: "AIzaSyCFYiYzNmXIylfkOoeODsQMq0LK5-0k-sM",
+    authDomain: "imj-first-project.firebaseapp.com",
+    databaseURL: "https://imj-first-project.firebaseio.com",
+    projectId: "imj-first-project",
+    storageBucket: "",
+    messagingSenderId: "804273504480",
+    appId: "1:804273504480:web:4f34d7dbd37a9a80"
+  }
+
+firebase.initializeApp(Config)
+
